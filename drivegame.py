5fd69adc95c91cf75e9884f779432cd76ce93762
m253516 <115736461+m253516@users.noreply.github.com>
2022-12-08 02:24:38 -0500

    added image/music
    final game

--- drivegame.py
+++ drivegame.py
@@ -1,7 +1,6 @@
 import pygame
 from pygame.locals import *
 import random
-import time
 import button
 
 pygame.init()
@@ -9,7 +8,11 @@ pygame.init()
 size = width, height = (400,800)
 screen = pygame.display.set_mode(size)
 
-# variables for pause screen
+# Music
+music = pygame.mixer.music.load('music/Killing my Love.mp3')
+pygame.mixer.music.play(-1)
+
+# variables for menu screen
 main_menu = True
 
 # variables for road
@@ -23,21 +26,22 @@ speed = 1
 pygame.display.update()
 
 # load player vehicle
-car = pygame.image.load('images/car_player.png')
+car = pygame.image.load('images/car_p1.png')
 car_loc = car.get_rect()
-car_loc.center = right_lane, height*0.8
+car_loc.center = right_lane, height*0.9
+
+car3 = pygame.image.load('images/car_p2.png')
+car3_loc = car.get_rect()
+car3_loc.center = left_lane, height*0.8
 
 # load enemy vehicle
-car2 = pygame.image.load('images/car_player.png')
+car2 = pygame.image.load('images/barrel_red_down.png')
 car2_loc = car.get_rect()
 car2_loc.center = left_lane, height*0.2
 
 # load button images
 start_img = pygame.image.load("images/start_btn.png")
 exit_img = pygame.image.load("images/exit_btn.png")
-
-#load level up image
-levelup_img = pygame.image.load("images/level_up.png")
 
 # create button instances
 start_button = button.Button(width/8, height/2, start_img, 0.5)
@@ -46,19 +50,12 @@ exit_button = button.Button(9*width/16, height/2, exit_img, 0.5)
 # define fonts
 font1 = pygame.font.SysFont('arialblack', 40)
 
-# # define font colors
-# TEXT_COL = (255, 255, 255)
-# def draw_text(text, font, text_col, x, y):
-#     level_up = font.render(text, True, text_col)
-#     screen.blit(level_up, (x,y))
-
-
 counter = 0
 # game loop
 running = True
 while running:
 
-    bg_color = green = (60, 220, 0)
+    bg_color = sand = (246, 215, 176)
     screen.fill(bg_color)
 
     # drawing the buttons
@@ -77,20 +74,18 @@ while running:
             else:
                 car2_loc.center = left_lane, -200
 
-        counter += 5
-        if counter == 5000:
-            speed += 0.2
+        # increase game difficulty as counter increases
+        counter += 1
+        print(counter)
+        if counter == 6000:
+            speed += 0.1
             counter = 0
             # print("level up", speed)
-            # screen.blit(level_up, (100, 100))
 
         # draw graphics
         grey = (50, 50, 50)
         pygame.draw.rect(
             screen, grey, (width / 2 - road_w / 2, 0, road_w, height))
-
-        # level up
-        # screen.blit(level_up, (100, 100))
 
         # center line
         pygame.draw.rect(screen, (255, 240, 60), (width / 2 - roadmark_w / 2, 0, roadmark_w, height))
@@ -103,19 +98,27 @@ while running:
 
         screen.blit(car, car_loc)
         screen.blit(car2, car2_loc)
+        screen.blit(car3, car3_loc)
 
         # end game
-        if car_loc[0] == car2_loc[0] and car2_loc[1] > car_loc[1] - 250:
+        if car_loc[0] == car2_loc[0] and car2_loc[1] > car_loc[1] - 50:
+            print("Game Over")
+            break
+        if car3_loc[0] == car2_loc[0] and car2_loc[1] > car3_loc[1] - 50:
             print("Game Over")
             break
 
     # event listeners
     for event in pygame.event.get():
         if event.type == KEYDOWN:
-            if event.key in [K_a, K_LEFT]:
+            if event.key in [K_LEFT]:
                 car_loc = car_loc.move([-int(road_w/2), 0])
-            if event.key in [K_d, K_RIGHT]:
+            if event.key in [K_RIGHT]:
                 car_loc = car_loc.move([+int(road_w/2), 0])
+            if event.key in [K_a]:
+                car3_loc = car3_loc.move([-int(road_w / 2), 0])
+            if event.key in [K_d]:
+                car3_loc = car3_loc.move([+int(road_w / 2), 0])
         if event.type == QUIT:
             running = False
 
